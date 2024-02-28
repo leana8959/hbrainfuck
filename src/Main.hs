c910f7ca74f9d4051d39492ec4b603b4976b9735
Léana 江 <leana.jiang@icloud.com>
2024-02-28 15:35:24 +0100

    add: parse cli options using a library (optparse-applicative)

--- src/Main.hs
+++ src/Main.hs
@@ -5,20 +5,32 @@ import Data.Bifunctor (Bifunctor(bimap))
 import qualified Data.Text.IO as TIO
 import Eval (evalBFAst, initBFState)
 import GHC.IO.IOMode (IOMode(ReadMode))
-import System.Environment (getArgs)
+import Options.Applicative
+  (ParserInfo, argument, execParser, fullDesc, header, help, helper, info,
+  metavar, optional, progDesc, str, (<**>))
 import System.IO (openFile)
 import Text.Megaparsec (errorBundlePretty, runParser)
 
 import Parser (bfParserToEof)
 
+argsParser :: ParserInfo (Maybe String)
+argsParser = withInfo (p <**> helper)
+  where
+    withInfo = flip info
+      ( fullDesc
+        <> header "hbrainfuck - a simple interpreter for brainfuck"
+        <> progDesc "Interpretes a program written in the brainfuck language and show the result"
+      )
+    p = optional
+        (argument str (metavar "FILEPATH" <> help "Which file to read from"))
+
 main :: IO ()
 main = do
-  args <- getArgs
+  file <- execParser argsParser
 
-  content <- case args of
-    (fname : _) | fname /= "-" ->
-      openFile fname ReadMode >>= TIO.hGetContents
-    _ -> TIO.getContents
+  content <- case file of
+    Just fname -> openFile fname ReadMode >>= TIO.hGetContents
+    Nothing    -> TIO.getContents
 
   state <- initBFState
   let output = bimap errorBundlePretty (($ state) . evalStateT . evalBFAst)
@@ -26,8 +38,5 @@ main = do
              $ content
 
   case output of
-    Right resultState -> do
-        result <- resultState
-        putStrLn result
-        putStrLn "The program has ended"
-    Left err          -> putStrLn $ "failed because " <> err
+    Right resultState -> putStrLn =<< resultState
+    Left err -> putStrLn $ "failed because " <> err
